d61cd0cf4a8d2e4560a40a82c3860bf9c8abc365
NA <node158@gmail.com>
2020-01-17 10:12:11 +0200

    Create setattr_test.py

---
+++ setattr_test.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+
+class Object:
+    attr1: None
+    attr2: None
+    attr3: None
+
+
+if __name__ == '__main__':
+    params = {'attr1': '1', 'attr2': '2', 'attr3': '3'}
+    obj = Object()
+    for p in params:
+        setattr(obj, p, params[p])
+    
+    print(obj.attr1)
